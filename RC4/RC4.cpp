a20cafa00fcde32d404105b890461762d4b6db89
Samuel Ramos Barroso <samuelrb1@gmail.com>
2016-03-02 14:58:12 +0000

    comenzando P3

---
+++ RC4/RC4.cpp
@@ -0,0 +1,6 @@
+#include "RC4.h"
+
+RC4_t::RC4_t(void)
+{}
+RC4_t::~RC4_t(void)
+{}
---
+++ RC4/RC4.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class RC4_t
+{
+    private:
+        
+        
+    public:
+        
+        RC4_t(void);
+        ~RC4_t(void);
+};
