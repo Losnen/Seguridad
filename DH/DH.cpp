36c96da446a59b380f49de8be400bb4d2ca1b947
Samuel Ramos Barroso <samuelrb1@gmail.com>
2016-04-04 17:30:22 +0000

    Constructores implementados

--- DH/DH.cpp
+++ DH/DH.cpp
@@ -1,1 +1,38 @@
 #include "DH.h"
+
+DH(void):
+p(0),
+alpha(0),
+k(0),
+Xa(0),
+Xb(0),
+Ya(0),
+Yb(0)
+{}
+
+DH(int P, int ALPHA, int K, int XA, int XB, int YA, int YB):
+p(P),
+alpha(ALPHA),
+k(K),
+Xa(XA),
+Xb(XB),
+Ya(YA),
+Yb(YB)
+{}
+
+~DH(void)
+{}
+        
+void generador(void)
+{
+    
+}
+void exponenciacion_rapida(int base, int ex)
+{
+    
+}
+
+ostream& operator<<(ostream& os)
+{
+    
+}
